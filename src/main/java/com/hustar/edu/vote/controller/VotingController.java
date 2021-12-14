4367c852212f2c545600cd41cbf82c87c98d5f81
dongchaebaek <ehdco8988@gmail.com>
2021-12-14 21:10:33 +0900

    투표선택DB저장

--- src/main/java/com/hustar/edu/vote/controller/VotingController.java
+++ src/main/java/com/hustar/edu/vote/controller/VotingController.java
@@ -1,12 +1,17 @@
 package com.hustar.edu.vote.controller;
 
+import com.hustar.edu.vote.auth.PrincipalDetail;
 import com.hustar.edu.vote.dto.tb_user;
 import com.hustar.edu.vote.service.VoteServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -19,9 +24,19 @@ public class VotingController {
         log.info("get getVotingCount");
 
         int voteCount= voteServiceimpl.getVotingCount();
+
         System.out.println("voteCount : " + voteCount);
 
         model.addAttribute("voteCount", voteCount);
+
         return "/vote/voting/voting";
+    }
+
+    @PostMapping("/form")
+    public String updateVoting(@AuthenticationPrincipal PrincipalDetail principal, tb_user user, Model model) {
+        user.setIdx(principal.getIdx());
+        voteServiceimpl.getupdateVoting(user);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/hustar/edu/vote/service/VoteServiceImpl.java
+++ src/main/java/com/hustar/edu/vote/service/VoteServiceImpl.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class VoteServiceImpl implements VoteService {
@@ -14,8 +16,14 @@ public class VoteServiceImpl implements VoteService {
 
     @Override
     public int getVotingCount() {
-        log.info("get selectVoing");
-        return voteMapper.selectVoting();
+        log.info("get selectVoingCount");
+        return voteMapper.selectVotingCount();
+    }
+
+    @Override
+    public void getupdateVoting(tb_user user) {
+        log.info("get updateVoting");
+        voteMapper.updateVoting(user);
     }
 
 
