b33a83c21ed21868b80841fe1f549553a4681fe1
95Seo <ay9564@naver.com>
2022-01-05 17:12:42 +0900

    수정

--- src/main/java/com/hustar/edu/vote/ServletInitializer.java
+++ src/main/java/com/hustar/edu/vote/ServletInitializer.java
@@ -1,20 +1,12 @@
 package com.hustar.edu.vote;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServletInitializer.class);
+		return application.sources(VoteApplication.class);
 	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ServletInitializer.class);
-	}
-
 }
