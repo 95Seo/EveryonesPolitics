c8e375855f08933a7d128f5e43e9f671ab624ac7
95Seo <ay9564@naver.com>
2022-01-05 17:31:43 +0900

    수정

--- src/main/java/com/hustar/edu/vote/ServletInitializer.java
+++ src/main/java/com/hustar/edu/vote/ServletInitializer.java
@@ -1,12 +1,23 @@
 package com.hustar.edu.vote;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(VoteApplication.class);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServletInitializer.class);
+	}
+
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ServletInitializer.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
--- src/main/java/com/hustar/edu/vote/VoteApplication.java
+++ src/main/java/com/hustar/edu/vote/VoteApplication.java
@@ -1,26 +1,26 @@
-package com.hustar.edu.vote;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class VoteApplication extends SpringBootServletInitializer {
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(VoteApplication.class);
-	}
-
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:aws.yml";
-
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(VoteApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
-	}
-
-}
+//package com.hustar.edu.vote;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//public class VoteApplication extends SpringBootServletInitializer {
+//
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(VoteApplication.class);
+//	}
+//
+//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//			+ "classpath:application.yml,"
+//			+ "classpath:aws.yml";
+//
+//	public static void main(String[] args) {
+//		new SpringApplicationBuilder(VoteApplication.class)
+//				.properties(APPLICATION_LOCATIONS)
+//				.run(args);
+//	}
+//
+//}
 
