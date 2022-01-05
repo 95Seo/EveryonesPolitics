a97ab00a9309d306dede166875e24281855ac9a1
95Seo <ay9564@naver.com>
2022-01-05 15:18:40 +0900

    war테스트

--- src/main/java/com/hustar/edu/vote/ServletInitializer.java
+++ src/main/java/com/hustar/edu/vote/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.hustar.edu.vote;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,15 +12,9 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ServletInitializer.class);
 	}
-
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:aws.yml";
-
+	
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ServletInitializer.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(ServletInitializer.class);
 	}
 
 }
