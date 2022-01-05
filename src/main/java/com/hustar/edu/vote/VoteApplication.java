76f63832b0dbbb5e61a1435bfe7b9d9667e06bc9
95Seo <ay9564@naver.com>
2022-01-05 14:19:08 +0900

    war테스트

--- src/main/java/com/hustar/edu/vote/VoteApplication.java
+++ src/main/java/com/hustar/edu/vote/VoteApplication.java
@@ -3,9 +3,14 @@ package com.hustar.edu.vote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VoteApplication {
+public class VoteApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(VoteApplication.class);
+	}
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
